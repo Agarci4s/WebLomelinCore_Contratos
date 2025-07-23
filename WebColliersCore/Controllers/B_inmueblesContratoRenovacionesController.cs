80fb096558d1ca9e3c23be67e973917b6ca6b297
Armando Rodriguez <armandorf90@gmail.com>
2025-07-22 23:45:05 -0600

    Se cargan Models, Data, Controllers y Views de Renovación con Adela, solo es diseño, no trae funcionalidad para VoBo. del Cliente.

---
+++ WebColliersCore/Controllers/B_inmueblesContratoRenovacionesController.cs
@@ -0,0 +1,37 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using Microsoft.AspNetCore.Mvc;
+using WebColliersCore;
+using WebColliersCore.Data;
+using WebColliersCore.Models;
+using WebLomelinCore.Data;
+using WebLomelinCore.Models;
+
+namespace WebLomelinCore.Controllers
+{
+    public class B_inmueblesContratoRenovacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult RenovacionAdela()
+        {
+            #region Validación de permisos
+            var claims = HttpContext.User.Claims;
+            Menu menu = new Menu();
+            int IdUsuario = 0, idCartera = 0, tipoNivel = 1;//0 , 1-detalle,2-editar y detalle, 3 crear-eliminar, editar y detalle   
+            if (!menu.ValidaPermiso(System.Reflection.MethodBase.GetCurrentMethod(), ref IdUsuario, ref idCartera, ref tipoNivel, claims))
+                return Redirect("~/Home");
+            #endregion
+
+            DataInmueblesRenovaciones dataInmueblesRenovaciones = new();
+            DataInmuebles dataInmuebles = new();
+            RenovacionAdela renovacionAdela = dataInmueblesRenovaciones.GetById(idCartera, IdUsuario, 130569);
+            //renovacionAdela.b_inmuebles = dataInmuebles.Get(idCartera, IdUsuario, 130569);
+            //renovacionAdela.b_Inmuebles_Contrato_Distribucions = null;
+            return View(renovacionAdela);
+        }
+    }
+}
---
+++ WebColliersCore/Models/B_inmuebles_contrato_distribucion.cs
@@ -0,0 +1,11 @@
+﻿namespace WebLomelinCore.Models
+{
+    public class B_inmuebles_contrato_distribucion
+    {
+        public int Id { get; set; }
+        public string Uso { get; set; }
+        public double Superficie { get; set; }
+        public double PrecioM2 { get; set; }
+        public double PrecioSuperficie { get; set; }
+    }
+}
