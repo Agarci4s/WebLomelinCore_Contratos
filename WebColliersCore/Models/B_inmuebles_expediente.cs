282d393e81690fc6993f8dd2e771c2e07fa7b67d
Adrián García Solís <a.garcia.sol@hotmail.es>
2025-08-21 19:41:59 -0600

    Cambios Marco

---
+++ WebColliersCore/Models/B_inmuebles_expediente.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System.ComponentModel.DataAnnotations;
+using System;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using NPOI.SS.Formula.Functions;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using WebLomelinCore.Models;
+
+namespace WebColliersCore.Models
+{
+    public class B_inmuebles_expediente
+    {
+        public List<B_cg_tipo_expediente_contratos> b_cg_tipo_expediente_contratos { get; set; }
+
+        public B_inmuebles b_inmuebles { get; set; }
+
+
+    }
+
+}
+
